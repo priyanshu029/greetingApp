768b51fe3f7f4b3cc30f8ae254a4b431170265b6
priyanshu <priyanshushekhar029@gmail.com>
2020-09-17 21:51:17 +0530

    1st..

--- lib/final.dart
+++ lib/final.dart
@@ -30,23 +30,45 @@ class finalPage extends StatelessWidget {
                     ),
                     child: Center(child: new Column(
                       children: <Widget>[
-                        Expanded(
-                          child: Text(heading,
-                            textAlign: TextAlign.center, style:
-                            TextStyle(color: Colors.brown, fontSize: 100, fontWeight: FontWeight.bold),),
+                        Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(top:24.0,left: 16.0,right: 16.0),
+                              child: Text(heading,
+                                textAlign: TextAlign.center, style:
+                                TextStyle(color: Colors.brown, fontSize: 50, fontWeight: FontWeight.bold),),
+                            ),
+                          ],
                         ),
 
-                        Text(to,
-                          textAlign: TextAlign.center, style:
-                          TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),),
+                        Row(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(left:100.0,top: 8.0),
+                              child: Text(to,
+                                textAlign: TextAlign.center, style:
+                                TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),),
+                            ),
+                          ],
+                        ),
 
-                        /*Text(body,
-                          textAlign: TextAlign.center, style:
-                          TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),),
+                        Padding(
+                          padding: const EdgeInsets.only(top:32.0,right: 45.0,left:45.0),
+                          child: Text(" O ut of the abstraction of nature comes its productivity Out of the abstraction of nature comes its productivity, and the production of a surplus over and above the necessitiesof survival. Out of this expanding surplus over necessity comes an expanding capacity to hack, again and again, pro-",
+                            textAlign: TextAlign.center, style:
+                            TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),),
+                        ),
 
-                        Text(from,
-                          textAlign: TextAlign.center, style:
-                          TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),)*/
+                        Expanded(
+                            child:Align(
+                              alignment: Alignment.bottomRight,
+                              child: Padding(
+                                padding: const EdgeInsets.only(top:8.0,bottom:16.0,left:8.0,right:8.0),
+                                child: Text(from,
+                                  textAlign: TextAlign.center, style:
+                                  TextStyle(color: Colors.brown, fontSize: 25, fontWeight: FontWeight.bold),),
+                              ),
+                            ))
                       ],
                     ),)
                 )
